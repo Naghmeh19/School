6764139598157aef17a0f4215655b1f636d91209
Naghmeh19 <sal19867@spengergasse.at>
2020-04-23 14:43:20 +0200

    Create School class

---
+++ src/School.java
@@ -0,0 +1,23 @@
+
+public class School {
+ private int anzahl;
+ private String name;
+public School(int anzahl, String name) {
+	super();
+	this.anzahl = anzahl;
+	this.name = name;
+}
+public int getAnzahl() {
+	return anzahl;
+}
+public void setAnzahl(int anzahl) {
+	this.anzahl = anzahl;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+ 
+}
